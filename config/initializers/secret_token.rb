3ec9ef2bcee888666b123a301529cd7d9bb05b4a
George Mendoza <gsmendoza@gmail.com>
2012-09-29 11:51:49 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movieaday::Application.config.secret_token = '4c30c61fac7c5517fb15d97e514e11bfa8dbfa7bf3502c38386d2689874df9a8925e5d900f82b3fd639081bc61fc2eb15e583b9f4957719c5e7c542415a31f7b'
